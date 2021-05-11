e797bcb99cc7f6603b8f1a95112c5f6c57095f05
AntoineKoiko <antoine.couacault@epitech.eu>
2021-05-11 17:26:22 +0200

    feat(Cmakelist.txt): compile hello project and dl / compilae raylib

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-REN-4-1-indiestudio-pol.bachelin
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello from bomberman" << std::endl;
+    return 0;
+}
