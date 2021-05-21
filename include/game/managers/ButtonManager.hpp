3fac23d2b1f4c79ac4702268aea48fcd064fa4ad
Pol Bachelin <pol.bachelin@epitech.eu>
2021-05-21 09:49:30 +0200

    chore(): commiting to go make vector class

---
+++ include/game/managers/ButtonManager.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-REN-4-1-indiestudio-pol.bachelin
+** File description:
+** ButtonManager
+*/
+
+#ifndef BUTTONMANAGER_HPP_
+#define BUTTONMANAGER_HPP_
+
+class ButtonManager {
+    public:
+        ButtonManager();
+        ~ButtonManager();
+
+        bool isButtonClicked();
+    protected:
+    private:
+};
+
+#endif /* !BUTTONMANAGER_HPP_ */
