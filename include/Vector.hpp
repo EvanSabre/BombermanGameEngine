23b04019934269e52eb117dfc816e1ab335f57a0
Pol Bachelin <pol.bachelin@epitech.eu>
2021-05-25 15:11:10 +0200

    fix(): fixed vector template being in cpp put all code to hpp

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -11,15 +11,42 @@
 template<typename T>
 class Vector {
     public:
-        Vector(T x, T y);
+        Vector(T x, T y) {
+            _x = x;
+            _y = y;
+        }
         ~Vector() = default;
         template<typename T> Vector(const Vector<T> &copy);
 
-        bool operator==(Vector<T> const &) const;
-        bool operator!=(Vector<T> const &) const;
-        Vector operator+(Vector<T> const &);
-        Vector operator-(Vector<T> const &);
-        Vector &operator=(Vector<T> const &);
+        bool operator==(Vector<T> const &) const {
+            if (_x == other._x && _y == other._y)
+                return true;
+            return false;
+        }
+        bool operator!=(Vector<T> const &) const {
+            if (_x != other._x && _y != other._y)
+                return true;
+            return false;
+        }
+        Vector operator+(Vector<T> const &) {
+            Vector<T> newVec(_x, _y);
+
+            newVec._x += other._x;
+            newVec._y += other._y;
+            return newVec;
+        }
+        Vector operator-(Vector<T> const &) {
+            Vector<T> newVec(_x, _y);
+
+            newVec._x -= other._x;
+            newVec._y -= other._y;
+            return newVec;
+        }
+        Vector &operator=(Vector<T> const &) {
+            _x = other._x;
+            _y = other._y;
+            return &this;
+        }
         T _x;
         T _y;
 };
