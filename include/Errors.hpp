92e9d9969d2898965927e45d8bb71d8838839731
AntoineKoiko <antoine.couacault@epitech.eu>
2021-05-31 15:28:11 +0200

    feat(Errors.cpp/hpp): Create basic custom Errors

---
+++ include/Errors.hpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-REN-4-1-indiestudio-pol.bachelin
+** File description:
+** Errors
+*/
+
+#ifndef ERRORS_HPP_
+#define ERRORS_HPP_
+
+#include <string>
+#include <exception>
+#include <stdexcept>
+
+
+
+class IndeeError : public std::exception
+{
+public:
+    IndeeError(std::string const &message, std::string const &component = "Unknown");
+
+    const std::string &getComponent() const noexcept;
+    const char *what() const noexcept override;
+
+private:
+    std::string _message;
+    std::string _componnent;
+};
+
+class LoadingError : public IndeeError
+{
+public:
+    LoadingError(std::string const &message,
+                         std::string const &component = "Unknown");
+};
+
+#endif /* !ERRORS_HPP_ */
