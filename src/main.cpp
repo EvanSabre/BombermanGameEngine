15f8741061d272a14d36ae4ba37b5ad94d75e2d3
EvanSabre <evansabre@hotmail.fr>
2021-06-01 15:04:24 +0200

    fix(): CMAKE avec Boost

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,52 @@
-/*
-** EPITECH PROJECT, 2021
-** B-YEP-400-REN-4-1-indiestudio-pol.bachelin
-** File description:
-** main
-*/
+// #include <fstream>
 
-#include <iostream>
-#include "raylib.h"
-#include "Button.hpp"
+// #include <boost/archive/text_oarchive.hpp>
+// #include <boost/archive/text_iarchive.hpp>
 
-#define NUM_FRAMES 1
+// class Note
+// {
+//         private:
+//                 friend class boost::serialization::access;
 
-    // Initialization
-    //--------------------------------------------------------------------------------------
-const int screenWidth = 800;
-const int screenHeight = 450;
+//                 template<class Archive>
+//                 void serialize(Archive & ar, const unsigned int version) {
+//                         (void) version;
+//                         ar & numerateur;
+//                         ar & denominateur;
+//                         ar & name;
+//                 }
+//                 int numerateur;
+//                 int denominateur;
+//                 std::string name;
 
-void run()
-{
-    Vector2 mousePoint = { 0.0f, 0.0f };
-    Texture2D button = LoadTexture("assets/button_test.png");
-    Vector<float> pos(screenWidth/2.0f - button.width/2.0f, screenHeight/2.0f - button.height/2.0f);
+//         public:
+//                 Note() {};
+//                 Note(int n, int d, std::string name) : numerateur(n), denominateur(d), name(name) {}
+// };
 
-    float frameHeight = (float)button.height/NUM_FRAMES;
-    Rectangle sourceRec = { 0, 0, (float)button.width, frameHeight };
 
-    // Define button bounds on screen
-    Rectangle btnBounds = { screenWidth/2.0f - button.width/2.0f, screenHeight/2.0f - button.height/NUM_FRAMES/2.0f, (float)button.width, frameHeight };
-    gameEngine::encapsulation::BText text("Press me", {pos._x, pos._y}, WHITE, 64);
-    gameEngine::encapsulation::Button but({button.width, button.height}, pos, text, BLACK);
-    SetTargetFPS(60);
-    //--------------------------------------------------------------------------------------
+// int main()
+// {
+//         std::ofstream ofs("fichierDeSerialisation");
 
-    // Main game loop
-     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        mousePoint = GetMousePosition();
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        but.drawButton();
-        Vector<float> mousP(mousePoint.x, mousePoint.y);
-        but.update();
-        if (but.getState() == gameEngine::encapsulation::Button::State::PRESSED)
-            but.setContentStr("I have been Pressed\n");
-        else
-            but.setContentStr("Press me!!!\n");
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-}
+//         // Vous avez vu comme je travaille bien ? :)
+//         const Note maNoteDePhysisque(20, 20, "Totosdf");
+//         {
+//                 boost::archive::text_oarchive oa(ofs);
+//                 oa << maNoteDePhysisque;
+//         }
 
-int main(void)
-{
-    InitWindow(screenWidth, screenHeight, "raylib [textures] example - sprite button");
+//         /** Quelque temps plus tard… ***/
 
-    InitAudioDevice();      // Initialize audio device
-    run();
-    CloseAudioDevice();     // Close audio device
-    CloseWindow();          // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-    return 0;
-}
+//         Note monAncienneNote;
+
+//         {
+//                 std::ifstream ifs("fichierDeSerialisation");
+//                 boost::archive::text_iarchive ia(ifs);
+
+//                 ia >> monAncienneNote;
+//         }
+//         //printf("Note: %d / %d\n", monAncienneNote.numerateur, monAncienneNote.denominateur);
+
+//         return 0;
+// }
