2ed07cc854b58a168fa130ac0f86d5c9129c054e
pol.bachelin@epitech.eu <pol.bachelin@epitech.eu>
2021-06-04 19:03:57 +0200

    feat(): continued puml with GO and systems

---
+++ include/gameEngine/systems/BoxCollision.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2021
+** Indie_Studio
+** File description:
+** Collision
+*/
+
+#ifndef COLLISION_HPP_
+#define COLLISION_HPP_
+
+#include "BBoundingBox.hpp"
+
+namespace gameEngine {
+    namespace system {
+        class BoxCollision {
+            public:
+                BoxCollision();
+                ~BoxCollision();
+            private:
+        };
+    }
+}
+
+#endif /* !COLLISION_HPP_ */
