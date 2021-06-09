a448052254dc0e6570ed41b555c97aa7547e2b05
pol.bachelin@epitech.eu <pol.bachelin@epitech.eu>
2021-06-09 13:45:32 +0200

    feat(): PlayGameScene to demonstrate switching

---
+++ include/game/scenes/PlayGameScene.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2021
+** Indie_Studio
+** File description:
+** PlayScene
+*/
+
+#ifndef PLAYGAMESCENE_HPP_
+#define PLAYGAMESCENE_HPP_
+
+#include "AScene.hpp"
+#include "ButtonManager.hpp"
+
+namespace game {
+    namespace scenes {
+        class PlayGameScene : public gameEngine::AScene {
+            public:
+                PlayGameScene(std::shared_ptr<gameEngine::managers::WindowManager> &windowManager, const gameEngine::scenes::SceneInfo &info);
+                ~PlayGameScene();
+
+                void start() override;
+                std::string update() override;
+            protected:
+            private:
+        };
+    }
+}
+
+#endif /* !PLAYSCENE_HPP_ */
