7a79afb40102126009519af66e5e6185ad84932e
simon.guyader@epitech.eu <simon.guyader@epitech.eu>
2021-06-17 11:07:51 +0200

    feat(AExplosif): added Explosif abstract class with Bomb, added timer thread to Bomb

---
+++ include/game/managers/ExplosionManager.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2021
+** indieStudio
+** File description:
+** ExplosionManager
+*/
+
+#ifndef EXPLOSIONMANAGER_HPP_
+#define EXPLOSIONMANAGER_HPP_
+
+class ExplosionManager {
+    public:
+        ExplosionManager();
+        ~ExplosionManager();
+
+    protected:
+    private:
+};
+
+#endif /* !EXPLOSIONMANAGER_HPP_ */
